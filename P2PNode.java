4917699a59fe2a322a993999e675120aac62d534
Alex Byrne <210148514@aston.ac.uk>
2026-01-28 19:49:32 +0000

    updates for name change + starting json builder

--- P2PNode.java
+++ P2PNode.java
@@ -1,34 +1,35 @@
 import java.io.*;
 import java.net.*;
+import java.time.Instant;
 import java.util.*;
+
+import org.json.JSONObject;
 import org.json.JSONObject.*;
 
 public class P2PNode {
-   // private static final String UUID = UUID.randomUUID().toString(); // this is temp before DHT
+    //private static final String UUID = UUID.randomUUID().toString(); // this is temp before DHT
+private static final String UUID = 
     // List to keep track of all the people we are talking to : Change to DHT 
     private static final List<PrintWriter> peers = Collections.synchronizedList(new ArrayList<>());
-    private static String username; // Store username globally
+    private static String username;
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("--- P2P Multi-User Node ---");
+        System.out.println("--- Welcome to Phoebe ---");
         System.out.print("Enter your username: ");
         username = scanner.nextLine();
-
         System.out.print("Enter the port you want to listen on (e.g. 6000): ");
         int myPort = Integer.parseInt(scanner.nextLine());
-
+      
         // Server Thread start
         new Thread(new ServerTask(myPort)).start();
-
         System.out.println("You are listening on port " + myPort);
         System.out.println("Connect to other user, type: /connect [IP] [PORT]");
-        System.err.println("To specify a user you wish to dm (JSON), type:/message [Username]");
+        System.out.println("To specify a user you wish to dm (JSON), type:/message [Username]");
+        System.out.println("When you wish to send an image, use /send_image");
         System.out.println("To send a message, just type it.");
         System.out.println("-------------------------------------------------");
 
         // USER INPUT While case
-
         while (true) {
             String input = scanner.nextLine();
 
@@ -42,7 +43,6 @@ public class P2PNode {
                 } else {
                     System.out.println("Must contain: [IP] [PORT] ");//[PORT]Usage: /connect [IP] [PORT]");
                 }
-            
             } else {
                 // Normal chat message - Send to ALL connected peers
                 broadcast("[" + username + "]: " + input);
@@ -56,19 +56,22 @@ public class P2PNode {
                     String reciverUUID = parts[1];
                     String JsonContent = parts[2];
                 }
-                    // Call builder here, 
+                    // Call builder here, add Image sending code input here 
             }
-
         }
     }
-/*
-Add Json Builder here
-
+//Add Json Builder here
+// Edit this all its ass . For those not me reading, crow = message Oculus = metadata of message
    public static String JsonBuilder(String senderUUID, String reciverUUID, String message){
         
+    JSONObject crow = new JSONObject();
+    crow.put("proginitor", senderUUID);
+    crow.put("message", message);
+    JSONObject Oculus = new JSONObject();
+    Oculus.put("timestamp", Instant.now().getEpochSecond());
+    Oculus.put("version", "1.0");
     }
 
-*/
     // Client thread connect-other
     private static void connectToPeer(String ip, int port) {
         try {
@@ -79,7 +82,6 @@ Add Json Builder here
             System.out.println("Failed to connect to " + ip + ":" + port);
         }
     }
-
     // Stores the output stream and starts a listener thread for a new socket
     private static void setupStreams(Socket socket) throws IOException {
         // Output stream: used to send messages TO this peer
